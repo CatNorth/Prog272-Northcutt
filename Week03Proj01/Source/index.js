8120f0b78f9aaf8cc80247123d3d715e3087da8f
unknown <CNorthcutt@N252-020.bellevuecollege.edu>
2014-01-21 18:08:08 -0800

    my assignment for week 3

---
+++ Week03Proj01/Source/index.js
@@ -0,0 +1,20 @@
+/**
+ * @author CNorthcutt
+ */
+function getNine(){
+	return 9;
+};
+function getEight(){
+	return 8;
+};
+
+
+ var numberGetter ={
+ 	getNine: function (){
+	return 9;
+},
+
+getEight: function (){
+	return 8;
+}
+};
---
+++ Week03Proj01/spec/test01.spec.js
@@ -0,0 +1,18 @@
+describe("Elvenware Jasmine One Suite", function() {
+  it("expects to get nine", function() {
+    expect(getNine()).toBe(9);
+  });
+  
+   it("expects to get eight", function() {
+    expect(getEight()).toBe(8);
+  });
+  
+     it("expects to get eight", function() {
+    expect(numberGetter.getEight()).toBe(8);
+  });
+  
+     it("expects to get nine", function() {
+    expect(numberGetter.getNine()).toBe(9);
+  });
+ 
+});
